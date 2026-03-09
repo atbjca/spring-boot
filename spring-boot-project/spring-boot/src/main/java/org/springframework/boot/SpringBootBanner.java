543cd9bb11a9cc06fe4f6ea5e91b00cda3b64c38
libiao <40131953+libiaolibiaolibiaobjca@users.noreply.github.com>
2026-03-09 17:47:11 +0800

    删除默认 banner

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/SpringBootBanner.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/SpringBootBanner.java
@@ -30,12 +30,8 @@ import org.springframework.core.env.Environment;
  */
 class SpringBootBanner implements Banner {
 
-	private static final String[] BANNER = { "", "  .   ____          _            __ _ _",
-			" /\\\\ / ___'_ __ _ _(_)_ __  __ _ \\ \\ \\ \\", "( ( )\\___ | '_ | '_| | '_ \\/ _` | \\ \\ \\ \\",
-			" \\\\/  ___)| |_)| | | | | || (_| |  ) ) ) )", "  '  |____| .__|_| |_|_| |_\\__, | / / / /",
-			" =========|_|==============|___/=/_/_/_/" };
-
-	private static final String SPRING_BOOT = " :: Spring Boot :: ";
+	private static final String[] BANNER = {""};
+	private static final String SPRING_BOOT = " :: Bpring Boot :: ";
 
 	private static final int STRAP_LINE_SIZE = 42;
 
